3c51170d75e3ec74f3ac3148063ac8de3430beed
youroot11 <youroot@mail.ru>
2025-03-31 01:36:12 +0300

    BaseTest.java

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -1,4 +1,20 @@
-package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://only.digital/");
+        try {
+            WebElement elementByXPath = driver.findElement(By.xpath("/html/body/main/footer")); // Пример XPath
+            System.out.println("Элемент найден: " + elementByXPath.getText());
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            System.out.println("Элемент не найден.");
+        }
+        driver.quit();
+    }
 }
