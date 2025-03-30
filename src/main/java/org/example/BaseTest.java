a57b7f41735a9e729e1b335ff5a34733feeed6f7
youroot11 <youroot@mail.ru>
2025-03-31 01:39:24 +0300

    BaseTest.java

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -4,15 +4,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
-
     public static void main(String[] args) {
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://only.digital/");
-        try {
-        WebElement elementByXPath = driver.findElement(By.xpath("/html/body/main/footer"));
+        try {WebElement elementByXPath = driver.findElement(By.xpath("/html/body/main/footer"));
             System.out.println("Элемент найден: " + elementByXPath.getText());
-        } catch (org.openqa.selenium.NoSuchElementException e) {
+        } 
+        catch (org.openqa.selenium.NoSuchElementException e) {
             System.out.println("Элемент не найден.");
         }
         driver.quit();
