44ba0af5027b053974bab53ae876613a9241b1ef
youroot11 <youroot@mail.ru>
2025-03-31 01:47:16 +0300

    BaseTest.java

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -4,13 +4,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
+
     public static void main(String[] args) {
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://only.digital/");
-        try {WebElement elementByXPath = driver.findElement(By.xpath("/html/body/main/footer"));
-            System.out.println("Элемент найден: " + elementByXPath.getText());
-        } 
-        catch (org.openqa.selenium.NoSuchElementException e) {
+        try {
+            WebElement elementByXPath = driver.findElement(By.xpath("/html/body/main/footer"));
+            System.out.println("Элемент 1 найден: " + elementByXPath.getText());
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            System.out.println("Элемент не найден.");
+        }
+        try {
+            WebElement elementByXPath = driver.findElement(By.xpath("html/body/main/footer/div[1]"));
+            System.out.println("Элемент 2 найден: " + elementByXPath.getText());
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            System.out.println("Элемент не найден.");
+        }
+        try {
+            WebElement elementByXPath = driver.findElement(By.xpath("html/body/main/footer/div[1]/button"));
+            System.out.println("Элемент 3 найден: " + elementByXPath.getText());
+        } catch (org.openqa.selenium.NoSuchElementException e) {
             System.out.println("Элемент не найден.");
         }
         driver.quit();
