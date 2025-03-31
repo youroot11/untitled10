bff4b49ef2b00415cea797f6da7803da5c68f0d4
youroot11 <youroot@mail.ru>
2025-03-31 06:42:39 +0300

    BaseTest.java

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class BaseTest {
 
@@ -26,6 +29,19 @@ public class BaseTest {
             System.out.println("Элемент 3 найден: " + elementByXPath.getText());
         } catch (org.openqa.selenium.NoSuchElementException e) {
             System.out.println("Элемент не найден.");
+        }
+        try {
+            WebElement elementByXPath = driver.findElement(By.xpath("html/body/main/footer/div[1]/button/ук32к432е34е"));
+            System.out.println("Элемент 4 найден: " + elementByXPath.getText());
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            System.out.println("Элемент 4 не найден.");
+        }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            WebElement elementToBePresent = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("html/body/main/footer/div[1]/button/ук32к432е34е")));
+            System.out.println("Элемент, ожидаемый WebDriverWait, найден: " + elementToBePresent.getTagName());
+        } catch (org.openqa.selenium.TimeoutException e) {
+            System.out.println("Элемент не появился в течение 10 секунд.");
         }
         driver.quit();
     }
